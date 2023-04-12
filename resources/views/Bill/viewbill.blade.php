01f0564b9bcc1bbcf77d6a789e81022781826c09
sukumarinapp <sukumar.inapp@gmail.com>
2023-04-12 11:33:49 +0530

    purchase

--- resources/views/Bill/viewbill.blade.php
+++ resources/views/Bill/viewbill.blade.php
@@ -3,10 +3,10 @@
    <thead>
       <tr><td style="text-align: center" colspan="5">Nalavariam</td></tr>
       <tr><td colspan="5">&nbsp;</td></tr>
-      <tr><td style="text-align: right">Bill# {{ $billnum }}</td><td></td><td>{{ $cust_name }}</td><td>{{ $mobile }}</td><td style="text-align: right">{{ $bill_date }}</td></tr>
+      <tr><td colspan="2" style="text-align: left">{{ $cust_name }}</td><td colspan="2" style="text-align: right">{{ $mobile }}</td></tr>
+      <tr><td colspan="2" style="text-align: left">Bill No: {{ $billnum }}</td><td colspan="2" style="text-align: right">Date : {{ $bill_date }}</td></tr>
       <tr><td colspan="5">&nbsp;</td></tr>
       <tr>
-         <th style="text-align: left">#</th>
          <th style="text-align: left">Item</th>
          <th style="text-align: right">Rate</th>
          <th style="text-align: right">Qty</th>
@@ -16,7 +16,6 @@
    <tbody>
       @foreach($bill as $key=>$b)
       <tr>
-         <td style="text-align: left">{{ $key + 1 }}</td>
          <td style="text-align: left">{{ $b->name }}</td>
          <td style="text-align: right">{{ $b->item_rate }}</td>
          <td style="text-align: right">{{ $b->quantity }}</td>
